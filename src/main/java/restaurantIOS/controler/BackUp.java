ad725473f035ab5ad1374e24580361532bb655b8
damnjan.markovic@hotmail.com <Minhen2020>
2020-04-17 23:50:12 +0200

    Updated database URL

---
+++ src/main/java/restaurantIOS/controler/BackUp.java
@@ -0,0 +1,7 @@
+package restaurantIOS.controler;
+
+public class BackUp {
+    /*
+    spring.datasource.url=jdbc:mysql://localhost:3306/restaurantIOS?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC
+     */
+}
